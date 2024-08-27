f345c37f7ec3701af7b96f54b22986b67ec5f0cd
FabiansDock <fabian20122001@gmail.com>
2024-08-27 11:07:35 +0530

    Wrote unit tests to retrieve collections

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,5 +1,7 @@
 from rest_framework import status
 import pytest
+from model_bakery import baker
+from store.models import Collection
 
 
 @pytest.fixture
@@ -7,6 +9,13 @@ def create_collection(api_client):
     def do_create_collection(collection):
         return api_client.post('/store/collections/', collection)
     return do_create_collection
+
+
+@pytest.fixture
+def retrieve_collection(api_client):
+    def do_retrieve_collection(collection):
+        return api_client.get(f'/store/collections/{collection}/')
+    return do_retrieve_collection
 
 
 @pytest.mark.django_db
@@ -18,12 +27,14 @@ class TestCreateCollections:
 
     def test_if_user_is_not_admin_returns_403(self, create_collection, authenticate):
         authenticate()
+
         response = create_collection({'title': 'a'})
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
     def test_if_data_is_invalid_returns_400(self, create_collection, authenticate):
         authenticate(is_staff=True)
+
         response = create_collection({'title': ''})
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
@@ -31,7 +42,23 @@ class TestCreateCollections:
 
     def test_if_data_is_valid_returns_201(self, create_collection, authenticate):
         authenticate(is_staff=True)
+
         response = create_collection({'title': 'a'})
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
+
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_valid_collection_returns_200(self, retrieve_collection):
+        collection = baker.make(Collection)
+
+        response = retrieve_collection(collection.id)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }
